9c5dd5e0cde72f169e263baa7372139be43c43ac
John Brock <john.brock@pillpack.com>
2024-12-10 17:32:50 +0000

    add linting, remove build files, update run scripts

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dayOne_1 = require("./dayOne");
-console.log('Day 1.1:', (0, dayOne_1.dayOnePartOne)(dayOne_1.day1Input));
-console.log('Day 1.2:', (0, dayOne_1.dayOnePartTwo)(dayOne_1.day1Input));
