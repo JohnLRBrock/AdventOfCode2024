3e0fe711c2ff17b63fd8994e0545bff998603bea
John Brock <john.brock@pillpack.com>
2024-12-10 17:41:17 +0000

    refactor input to have a single method for every day

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import * as DayOne from './dayOne'
 import * as DayTwo from './dayTwo';
 import * as Input from './input'
 
-console.log('Day 1.1:', DayOne.partOne(Input.day1()));
-console.log('Day 1.2:', DayOne.partTwo(Input.day1()));
-console.log('Day 2.1:', DayTwo.partOne(Input.day2()));
+console.log('Day 1.1:', DayOne.partOne(Input.day(1)));
+console.log('Day 1.2:', DayOne.partTwo(Input.day(1)));
+console.log('Day 2.1:', DayTwo.partOne(Input.day(2)));
--- src/input.ts
+++ src/input.ts
@@ -1,13 +1,9 @@
 "use strict";
 
-export const fetch = (fileUrl: string): string => {
+export const day = (n: Number): string => {
+    return fetch(`./input/day${n}.txt`);
+}
+
+const fetch = (fileUrl: string): string => {
     return require('fs').readFileSync(fileUrl, 'utf8');
-}
-
-export const day1 = (): string => {
-    return fetch('./input/day1.txt');
-}
-
-export const day2 = (): string => {
-    return fetch('./input/day2.txt')
 }
