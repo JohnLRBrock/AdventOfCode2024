0acdacd7228e5a5eab58c84c4138f0fa4b8ad3f1
John Brock <john.brock@pillpack.com>
2024-12-11 22:36:53 +0000

    add day template an a semi colon

---
+++ src/day00.ts
@@ -0,0 +1,5 @@
+"use strict";
+// https://adventofcode.com/2024/day/
+
+export const partOne = (input: string) => {};
+export const partTwo = (input: string) => {};
--- src/day02.ts
+++ src/day02.ts
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 // adventofcode.com/2024/day/2
 
 import { toMatrix, everyPair } from './lib'
